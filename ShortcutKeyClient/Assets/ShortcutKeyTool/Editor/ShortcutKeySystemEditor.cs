1462056076ffafef8d21ef37523afc867af8bc44
liuzhenhua <liuzhenhua@putao.com>
2017-10-23 20:51:19 +0800

    0.0.0.5 Add assort.

--- ShortcutKeyClient/Assets/ShortcutKeyTool/Editor/ShortcutKeySystemEditor.cs
+++ ShortcutKeyClient/Assets/ShortcutKeyTool/Editor/ShortcutKeySystemEditor.cs
@@ -1,77 +1,317 @@
-﻿//using UnityEditor;
-//
-//public class ShortcutKeySystemEditor
-//{
-//		[MenuItem("LLL/ShortcutKey/Folder #n")]
-//		static void ShortcutKey_AssetsCreateFolder()
-//		{
-//			EditorApplication.ExecuteMenuItem("Assets/Create/Folder");
-//		}
-//
-//		[MenuItem("LLL/ShortcutKey/C#Script #&n")]
-//		static void ShortcutKey_AssetsCreateCScript()
-//		{
-//			EditorApplication.ExecuteMenuItem("Assets/Create/C# Script");
-//		}
-//
-//		[MenuItem("LLL/ShortcutKey/Javascript #&j")]
-//		static void ShortcutKey_AssetsCreateJavascript()
-//		{
-//			EditorApplication.ExecuteMenuItem("Assets/Create/Javascript");
-//		}
-//
-//		[MenuItem("LLL/ShortcutKey/UnlitShader #s")]
-//		static void ShortcutKey_AssetsCreateShaderUnlitShader()
-//		{
-//			EditorApplication.ExecuteMenuItem("Assets/Create/Shader/Unlit Shader");
-//		}
-//
-//		[MenuItem("LLL/ShortcutKey/EditorTestC#Script #&t")]
-//		static void ShortcutKey_AssetsCreateTestingEditorTestCScript()
-//		{
-//			EditorApplication.ExecuteMenuItem("Assets/Create/Testing/Editor Test C# Script");
-//		}
-//
-//		[MenuItem("LLL/ShortcutKey/Scene #&s")]
-//		static void ShortcutKey_AssetsCreateScene()
-//		{
-//			EditorApplication.ExecuteMenuItem("Assets/Create/Scene");
-//		}
-//
-//		[MenuItem("LLL/ShortcutKey/Prefab #b")]
-//		static void ShortcutKey_AssetsCreatePrefab()
-//		{
-//			EditorApplication.ExecuteMenuItem("Assets/Create/Prefab");
-//		}
-//
-//		[MenuItem("LLL/ShortcutKey/AudioMixer _m")]
-//		static void ShortcutKey_AssetsCreateAudioMixer()
-//		{
-//			EditorApplication.ExecuteMenuItem("Assets/Create/Audio Mixer");
-//		}
-//
-//		[MenuItem("LLL/ShortcutKey/Material #m")]
-//		static void ShortcutKey_AssetsCreateMaterial()
-//		{
-//			EditorApplication.ExecuteMenuItem("Assets/Create/Material");
-//		}
-//
-//		[MenuItem("LLL/ShortcutKey/LensFlare # ")]
-//		static void ShortcutKey_AssetsCreateLensFlare()
-//		{
-//			EditorApplication.ExecuteMenuItem("Assets/Create/Lens Flare");
-//		}
-//
-//		[MenuItem("LLL/ShortcutKey/RenderTexture ")]
-//		static void ShortcutKey_AssetsCreateRenderTexture()
-//		{
-//			EditorApplication.ExecuteMenuItem("Assets/Create/Render Texture");
-//		}
-//
-//		[MenuItem("LLL/ShortcutKey/LightmapParameters ")]
-//		static void ShortcutKey_AssetsCreateLightmapParameters()
-//		{
-//			EditorApplication.ExecuteMenuItem("Assets/Create/Lightmap Parameters");
-//		}
-//
-//}
+﻿using UnityEditor;
+
+public class ShortcutKeySystemEditor
+{
+		[MenuItem("LLL/ShortcutKey/NewScene ")]
+		static void ShortcutKey_FileNewScene()
+		{
+			EditorApplication.ExecuteMenuItem("File/New Scene");
+		}
+
+		[MenuItem("LLL/ShortcutKey/OpenScene ")]
+		static void ShortcutKey_FileOpenScene()
+		{
+			EditorApplication.ExecuteMenuItem("File/Open Scene");
+		}
+
+		[MenuItem("LLL/ShortcutKey/SaveScenes ")]
+		static void ShortcutKey_FileSaveScenes()
+		{
+			EditorApplication.ExecuteMenuItem("File/Save Scenes");
+		}
+
+		[MenuItem("LLL/ShortcutKey/SaveSceneas... ")]
+		static void ShortcutKey_FileSaveSceneas()
+		{
+			EditorApplication.ExecuteMenuItem("File/Save Scene as...");
+		}
+
+		[MenuItem("LLL/ShortcutKey/NewProject... ")]
+		static void ShortcutKey_FileNewProject()
+		{
+			EditorApplication.ExecuteMenuItem("File/New Project...");
+		}
+
+		[MenuItem("LLL/ShortcutKey/OpenProject... ")]
+		static void ShortcutKey_FileOpenProject()
+		{
+			EditorApplication.ExecuteMenuItem("File/Open Project...");
+		}
+
+		[MenuItem("LLL/ShortcutKey/SaveProject ")]
+		static void ShortcutKey_FileSaveProject()
+		{
+			EditorApplication.ExecuteMenuItem("File/Save Project");
+		}
+
+		[MenuItem("LLL/ShortcutKey/BuildSettings... ")]
+		static void ShortcutKey_FileBuildSettings()
+		{
+			EditorApplication.ExecuteMenuItem("File/Build Settings...");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Build&Run ")]
+		static void ShortcutKey_FileBuildRun()
+		{
+			EditorApplication.ExecuteMenuItem("File/Build & Run");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Close ")]
+		static void ShortcutKey_FileClose()
+		{
+			EditorApplication.ExecuteMenuItem("File/Close");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Undo ")]
+		static void ShortcutKey_EditUndo()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Undo");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Redo ")]
+		static void ShortcutKey_EditRedo()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Redo");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Cut ")]
+		static void ShortcutKey_EditCut()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Cut");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Copy ")]
+		static void ShortcutKey_EditCopy()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Copy");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Paste ")]
+		static void ShortcutKey_EditPaste()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Paste");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Duplicate ")]
+		static void ShortcutKey_EditDuplicate()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Duplicate");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Delete ")]
+		static void ShortcutKey_EditDelete()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Delete");
+		}
+
+		[MenuItem("LLL/ShortcutKey/FrameSelected ")]
+		static void ShortcutKey_EditFrameSelected()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Frame Selected");
+		}
+
+		[MenuItem("LLL/ShortcutKey/LockViewtoSelected ")]
+		static void ShortcutKey_EditLockViewtoSelected()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Lock View to Selected");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Find ")]
+		static void ShortcutKey_EditFind()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Find");
+		}
+
+		[MenuItem("LLL/ShortcutKey/SelectAll ")]
+		static void ShortcutKey_EditSelectAll()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Select All");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Play ")]
+		static void ShortcutKey_EditPlay()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Play");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Pause ")]
+		static void ShortcutKey_EditPause()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Pause");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Step ")]
+		static void ShortcutKey_EditStep()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Step");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Signin... ")]
+		static void ShortcutKey_EditSignin()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Sign in...");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Signout ")]
+		static void ShortcutKey_EditSignout()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Sign out");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Input #i")]
+		static void ShortcutKey_EditProjectSettingsInput()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Project Settings/Input");
+		}
+
+		[MenuItem("LLL/ShortcutKey/TagsandLayers #l")]
+		static void ShortcutKey_EditProjectSettingsTagsandLayers()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Project Settings/Tags and Layers");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Audio #a")]
+		static void ShortcutKey_EditProjectSettingsAudio()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Project Settings/Audio");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Time #t")]
+		static void ShortcutKey_EditProjectSettingsTime()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Project Settings/Time");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Player _p")]
+		static void ShortcutKey_EditProjectSettingsPlayer()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Project Settings/Player");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Physics #p")]
+		static void ShortcutKey_EditProjectSettingsPhysics()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Project Settings/Physics");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Physics2D #&p")]
+		static void ShortcutKey_EditProjectSettingsPhysics2D()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Project Settings/Physics 2D");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Quality #q")]
+		static void ShortcutKey_EditProjectSettingsQuality()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Project Settings/Quality");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Graphics #g")]
+		static void ShortcutKey_EditProjectSettingsGraphics()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Project Settings/Graphics");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Network _n")]
+		static void ShortcutKey_EditProjectSettingsNetwork()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Project Settings/Network");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Editor #e")]
+		static void ShortcutKey_EditProjectSettingsEditor()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Project Settings/Editor");
+		}
+
+		[MenuItem("LLL/ShortcutKey/ScriptExecutionOrder _s")]
+		static void ShortcutKey_EditProjectSettingsScriptExecutionOrder()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Project Settings/Script Execution Order");
+		}
+
+		[MenuItem("LLL/ShortcutKey/ClusterInput _c")]
+		static void ShortcutKey_EditProjectSettingsClusterInput()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Project Settings/Cluster Input");
+		}
+
+		[MenuItem("LLL/ShortcutKey/SnapSettings... #c")]
+		static void ShortcutKey_EditSnapSettings()
+		{
+			EditorApplication.ExecuteMenuItem("Edit/Snap Settings...");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Folder #n")]
+		static void ShortcutKey_AssetsCreateFolder()
+		{
+			EditorApplication.ExecuteMenuItem("Assets/Create/Folder");
+		}
+
+		[MenuItem("LLL/ShortcutKey/C#Script #&n")]
+		static void ShortcutKey_AssetsCreateCScript()
+		{
+			EditorApplication.ExecuteMenuItem("Assets/Create/C# Script");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Javascript #&j")]
+		static void ShortcutKey_AssetsCreateJavascript()
+		{
+			EditorApplication.ExecuteMenuItem("Assets/Create/Javascript");
+		}
+
+		[MenuItem("LLL/ShortcutKey/UnlitShader #s")]
+		static void ShortcutKey_AssetsCreateShaderUnlitShader()
+		{
+			EditorApplication.ExecuteMenuItem("Assets/Create/Shader/Unlit Shader");
+		}
+
+		[MenuItem("LLL/ShortcutKey/EditorTestC#Script #&t")]
+		static void ShortcutKey_AssetsCreateTestingEditorTestCScript()
+		{
+			EditorApplication.ExecuteMenuItem("Assets/Create/Testing/Editor Test C# Script");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Scene #&s")]
+		static void ShortcutKey_AssetsCreateScene()
+		{
+			EditorApplication.ExecuteMenuItem("Assets/Create/Scene");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Prefab #b")]
+		static void ShortcutKey_AssetsCreatePrefab()
+		{
+			EditorApplication.ExecuteMenuItem("Assets/Create/Prefab");
+		}
+
+		[MenuItem("LLL/ShortcutKey/AudioMixer _m")]
+		static void ShortcutKey_AssetsCreateAudioMixer()
+		{
+			EditorApplication.ExecuteMenuItem("Assets/Create/Audio Mixer");
+		}
+
+		[MenuItem("LLL/ShortcutKey/Material #m")]
+		static void ShortcutKey_AssetsCreateMaterial()
+		{
+			EditorApplication.ExecuteMenuItem("Assets/Create/Material");
+		}
+
+		[MenuItem("LLL/ShortcutKey/LensFlare # ")]
+		static void ShortcutKey_AssetsCreateLensFlare()
+		{
+			EditorApplication.ExecuteMenuItem("Assets/Create/Lens Flare");
+		}
+
+		[MenuItem("LLL/ShortcutKey/RenderTexture ")]
+		static void ShortcutKey_AssetsCreateRenderTexture()
+		{
+			EditorApplication.ExecuteMenuItem("Assets/Create/Render Texture");
+		}
+
+		[MenuItem("LLL/ShortcutKey/LightmapParameters ")]
+		static void ShortcutKey_AssetsCreateLightmapParameters()
+		{
+			EditorApplication.ExecuteMenuItem("Assets/Create/Lightmap Parameters");
+		}
+
+}
--- ShortcutKeyClient/Assets/ShortcutKeyTool/Editor/ShortcutWindowEditor.cs
+++ ShortcutKeyClient/Assets/ShortcutKeyTool/Editor/ShortcutWindowEditor.cs
@@ -12,9 +12,19 @@ public class ShortcutWindowEditor : EditorWindow
 {
 	private static string tipsLabelText = "* %->commond #->shift &->ctrl _a->keyboard[A]";
 	
+	private static string ShortcutSystemPath 
+	{
+		get { return Application.dataPath + "/ShortcutKeyTool/Editor/ShortcutKeySystemEditor.cs";}
+	}
+	
 	private static string ShortcutPath 
 	{
 		get { return Application.dataPath + "/ShortcutKeyTool/Editor/ShortcutKeyEditor.cs";}
+	}
+	
+	private static string ShortcutSystemSavePath 
+	{
+		get { return Application.dataPath + "/ShortcutKeyTool/Res/ShortcutSystemKeyData.xml";}
 	}
 
 	private static string ShortcutSavePath 
@@ -30,16 +40,25 @@ public class ShortcutWindowEditor : EditorWindow
 		myWindow.Show();
 	}
 
-	private ShortKeyCodeItem ski;
-	private List<ShortKeyCodeItem> mSkiList;
+	private ShortItem ski;
+	private List<ShortItem> mSkiList;
 	private Vector2 scrollPositon;
-	private bool folderOut;
+	private bool folderOut1;
+	private bool folderOut2;
+	private bool folderOut3;
+	private bool folderOut4;
+	private bool folderOut5;
+	private bool folderOut6;
+	private bool folderOut7;
+	private bool folderOut8;
+
+	private Dictionary<ShortType, List<ShortItem>> m_ShortKeyDict;
 	
 	void Awake()
 	{
-		ski=new ShortKeyCodeItem();
-		mSkiList = new List<ShortKeyCodeItem> ();
-		mSkiList = LoadShortcutKeyFromFile (ShortcutSavePath);
+		InitSystemData();
+		ski=new ShortItem("","");
+		mSkiList=new List<ShortItem>();
 		scrollPositon = new Vector2(300, 500);
 	}
 
@@ -55,37 +74,32 @@ public class ShortcutWindowEditor : EditorWindow
 		//tips
 		EditorGUILayout.LabelField(tipsLabelText);
 		GUILayout.Space(10);
-		folderOut = EditorGUILayout.Foldout(folderOut,"File");
-		if (folderOut)
-		{
-			//show
-			scrollPositon =EditorGUILayout.BeginScrollView(scrollPositon);
-			if (mSkiList!=null && mSkiList.Count > 0)
-			{
-				for (int i = 0; i < mSkiList.Count; i++)
-				{
-					ShowItem(mSkiList[i]);
-				}
-			}
-			EditorGUILayout.EndScrollView();
-		}
+		//Show
+		ShowList();
 		//button
 		EditorGUILayout.BeginHorizontal();
 		if(GUILayout.Button("add"))
 		{
-			ski=new ShortKeyCodeItem("Editor1/Editor2/Editor3","");
-			Debug.LogFormat("add {0} {1} {2} {3} ",ski.TargetName,ski.ShortKeyName,ski.MenuItemName,ski.FuncName);
-			if(mSkiList==null)
-				mSkiList=new List<ShortKeyCodeItem>();
-			mSkiList.Add(ski);
+			ski=new ShortItem("Editor1/Editor2/Editor3","");
+			Debug.LogFormat("add {0} {1} {2} {3} ",ski.Path,ski.ShortKey,ski.MenuItemName,ski.FuncName);
+			if(m_ShortKeyDict[ShortType.Extension]==null)
+				m_ShortKeyDict[ShortType.Extension]=new List<ShortItem>();
+			m_ShortKeyDict[ShortType.Extension].Add(ski);
 		}
 		if (GUILayout.Button("save"))
 		{
-			//TODO: save to disk!
+			foreach (ShortType st in m_ShortKeyDict.Keys)
+			{
+				if (st != ShortType.Extension)
+				{
+					mSkiList.AddRange(m_ShortKeyDict[st]);
+				}
+			}
+			SaveShortcutKey2File (ShortcutSystemSavePath,mSkiList);
+			SaveShortcutKey2EditorCode(ShortcutSystemPath,mSkiList);
 			//save to script
-			SaveShortcutKey2File (ShortcutSavePath,mSkiList);
-			SaveShortcutKey2EditorCode(ShortcutPath,mSkiList);
-			Debug.Log("Save!");
+			SaveShortcutKey2File (ShortcutSavePath,m_ShortKeyDict[ShortType.Extension]);
+			SaveShortcutKey2EditorCode(ShortcutPath,m_ShortKeyDict[ShortType.Extension]);
 		}
 		EditorGUILayout.EndHorizontal();
 		GUILayout.Space(10);
@@ -93,31 +107,133 @@ public class ShortcutWindowEditor : EditorWindow
 
 	void OnDestroy()
 	{
-		if (mSkiList != null)
+		if (m_ShortKeyDict == null)
 		{
-			mSkiList.Clear ();
-			mSkiList = null;
+			m_ShortKeyDict.Clear();
+			m_ShortKeyDict = null;
 		}
 	}
 
-	void ShowItem(ShortKeyCodeItem codeItem)
+	void InitSystemData()
+	{
+		if (m_ShortKeyDict == null)
+		{
+			m_ShortKeyDict=new Dictionary<ShortType, List<ShortItem>>()
+			{
+				{ShortType.File,LoadShortcutKeyFromFile (ShortcutSystemSavePath,ShortType.File)},
+				{ShortType.Edit,LoadShortcutKeyFromFile (ShortcutSystemSavePath,ShortType.Edit)},
+				{ShortType.Assets,LoadShortcutKeyFromFile (ShortcutSystemSavePath,ShortType.Assets)},
+				{ShortType.GameObject,LoadShortcutKeyFromFile (ShortcutSystemSavePath,ShortType.GameObject)},
+				{ShortType.Component,LoadShortcutKeyFromFile (ShortcutSystemSavePath,ShortType.Component)},
+				{ShortType.Window,LoadShortcutKeyFromFile (ShortcutSystemSavePath,ShortType.Window)},
+				{ShortType.Help,LoadShortcutKeyFromFile (ShortcutSystemSavePath,ShortType.Help)},
+				{ShortType.Extension,LoadShortcutKeyFromFile (ShortcutSystemSavePath,ShortType.Extension)}
+			};
+		}
+	}
+
+	void ShowList()
+	{
+		if (m_ShortKeyDict == null)
+		{
+			InitSystemData();
+		}
+		foreach (var sType in m_ShortKeyDict.Keys)
+		{
+			switch (sType)
+			{
+				case ShortType.File:
+					folderOut1 = EditorGUILayout.Foldout(folderOut1,ShortType.File.ToString());
+					if (folderOut1)
+					{
+						ShowItemList(ShortType.File);
+					}
+					break;
+				case ShortType.Edit:
+					folderOut2 = EditorGUILayout.Foldout(folderOut2,ShortType.Edit.ToString());
+					if (folderOut2)
+					{
+						ShowItemList(ShortType.Edit);
+					}
+					break;
+				case ShortType.Assets:
+					folderOut3 = EditorGUILayout.Foldout(folderOut3,ShortType.Assets.ToString());
+					if (folderOut3)
+					{
+						ShowItemList(ShortType.Assets);
+					}
+					break;
+				case ShortType.GameObject:
+					folderOut4 = EditorGUILayout.Foldout(folderOut4,ShortType.GameObject.ToString());
+					if (folderOut4)
+					{
+						ShowItemList(ShortType.GameObject);
+					}
+					break;
+				case ShortType.Component:
+					folderOut5 = EditorGUILayout.Foldout(folderOut5,ShortType.Component.ToString());
+					if (folderOut5)
+					{
+						ShowItemList(ShortType.Component);
+					}
+					break;
+				case ShortType.Window:
+					folderOut6 = EditorGUILayout.Foldout(folderOut6,ShortType.Window.ToString());
+					if (folderOut6)
+					{
+						ShowItemList(ShortType.Window);
+					}
+					break;
+				case ShortType.Help:
+					folderOut7 = EditorGUILayout.Foldout(folderOut7,ShortType.Help.ToString());
+					if (folderOut7)
+					{
+						ShowItemList(ShortType.Help);
+					}
+					break;
+				case ShortType.Extension:
+					folderOut8 = EditorGUILayout.Foldout(folderOut8,ShortType.Extension.ToString());
+					if (folderOut8)
+					{
+						ShowItemList(ShortType.Extension,false);
+					}
+					break;
+			}
+		}
+	}
+
+	void ShowItemList(ShortType sType,bool isSystem=true)
+	{
+		//show
+		scrollPositon =EditorGUILayout.BeginScrollView(scrollPositon);
+		if (m_ShortKeyDict[sType]!=null && m_ShortKeyDict[sType].Count > 0)
+		{
+			for (int i = 0; i < m_ShortKeyDict[sType].Count; i++)
+			{
+				ShowItem(m_ShortKeyDict[sType][i],m_ShortKeyDict[sType],isSystem);
+			}
+		}
+		EditorGUILayout.EndScrollView();
+	}
+
+	void ShowItem(ShortItem codeItem,List<ShortItem> list,bool isSystem=true)
 	{
 		EditorGUILayout.BeginHorizontal();
-		EditorGUILayout.LabelField("TargetName:",GUILayout.Width(85));
-		codeItem.TargetName = EditorGUILayout.TextField(codeItem.TargetName, GUILayout.Width(200));
+		EditorGUILayout.LabelField("Path:",GUILayout.Width(85));
+		codeItem.Path = EditorGUILayout.TextField(codeItem.Path, GUILayout.Width(200));
 		EditorGUILayout.LabelField("ShortcutName:",GUILayout.Width(85));
-		codeItem.ShortKeyName = EditorGUILayout.TextField(codeItem.ShortKeyName, GUILayout.Width(85));
-		if (GUILayout.Button("delete",GUILayout.Width(70),GUILayout.Height(20)))
+		codeItem.ShortKey = EditorGUILayout.TextField(codeItem.ShortKey, GUILayout.Width(85));
+		if (!isSystem && GUILayout.Button("delete",GUILayout.Width(70),GUILayout.Height(20)))
 		{
-			if (mSkiList.Contains(codeItem))
+			if (list.Contains(codeItem))
 			{
-				mSkiList.Remove(codeItem);
+				list.Remove(codeItem);
 			}
 		}
 		EditorGUILayout.EndHorizontal();
 	}
 	
-	private void SaveShortcutKey2EditorCode(string strFilePath,List<ShortKeyCodeItem> skiList)
+	private void SaveShortcutKey2EditorCode(string strFilePath,List<ShortItem> skiList)
 	{
 		if (!File.Exists(strFilePath))
 		{
@@ -144,10 +260,10 @@ public class ShortcutWindowEditor : EditorWindow
 			//方法开始
 			for (int i = 0; i < skiList.Count; i++)
 			{
-				strBuilder.Append("\t\t").AppendFormat("[MenuItem(\"LLL/ShortcutKey/{0} {1}\")]",skiList[i].MenuItemName,skiList[i].ShortKeyName).AppendLine();
+				strBuilder.Append("\t\t").AppendFormat("[MenuItem(\"LLL/ShortcutKey/{0} {1}\")]",skiList[i].MenuItemName,skiList[i].ShortKey).AppendLine();
 				strBuilder.Append("\t\t").AppendFormat("static void ShortcutKey_{0}()",skiList[i].FuncName).AppendLine();
 				strBuilder.Append("\t\t").AppendLine("{");
-				strBuilder.Append("\t\t").Append("\t").AppendFormat("EditorApplication.ExecuteMenuItem(\"{0}\");",skiList[i].TargetName).AppendLine();
+				strBuilder.Append("\t\t").Append("\t").AppendFormat("EditorApplication.ExecuteMenuItem(\"{0}\");",skiList[i].Path).AppendLine();
 				strBuilder.Append("\t\t").AppendLine("}").AppendLine();
 			}
 			//方法结束
@@ -165,7 +281,7 @@ public class ShortcutWindowEditor : EditorWindow
 		Debug.Log(">>>>>>>Success Create UIPrefab Code: " + strDlg);
 	}
 
-	private void SaveShortcutKey2File(string savePath,List<ShortKeyCodeItem> list)
+	private void SaveShortcutKey2File(string savePath,List<ShortItem> list)
 	{
 		if (!File.Exists(savePath))
 		{
@@ -177,27 +293,26 @@ public class ShortcutWindowEditor : EditorWindow
 		//创建子节点
 		for (int i = 0; i < list.Count; i++) 
 		{
-			Debug.Log (list [i].TargetName);
 			XmlElement element=xml.CreateElement("ShortcutKeyItem");
 			element.SetAttribute ("id", (i+1).ToString());
-			XmlElement elementChild1 = xml.CreateElement("TargetName");
-			elementChild1.InnerText = list [i].TargetName;
+			XmlElement elementChild1 = xml.CreateElement("Path");
+			elementChild1.InnerText = list [i].Path;
 			XmlElement elementChild2 = xml.CreateElement("ShortKeyName");
-			elementChild2.InnerText = list [i].ShortKeyName;
+			elementChild2.InnerText = list [i].ShortKey;
 			//把节点一层一层的添加至xml中，注意他们之间的先后顺序，这是生成XML文件的顺序
 			element.AppendChild(elementChild1);
 			element.AppendChild(elementChild2);
 			root.AppendChild(element);
 		}
 		xml.AppendChild (root);
-		xml.Save (savePath);
+		xml.Save(savePath);
 	}
 
-	private List<ShortKeyCodeItem> LoadShortcutKeyFromFile(string savePath)
+	private List<ShortItem> LoadShortcutKeyFromFile(string savePath,ShortType sType)
 	{
 		if (!File.Exists (savePath))
 			return null;
-		List<ShortKeyCodeItem> itemList = new List<ShortKeyCodeItem> ();
+		List<ShortItem> itemList = new List<ShortItem> ();
 		XmlDocument xml = new XmlDocument ();
 		xml.Load (savePath);
 		XmlNodeList xnlList = xml.SelectSingleNode ("KeyData").ChildNodes;
@@ -209,56 +324,16 @@ public class ShortcutWindowEditor : EditorWindow
 			{
 				foreach (XmlNode item in xel.ChildNodes) 
 				{
-					if (item.Name.Equals ("TargetName"))
+					if (item.Name.Equals ("Path"))
 						tn = item.InnerText;
 					if (item.Name.Equals ("ShortKeyName"))
 						sn = item.InnerText;
-				}					
-				itemList.Add (new ShortKeyCodeItem (tn,sn));
+				}
+				if (tn.Split('/')[0].Equals(sType.ToString()))
+					itemList.Add(new ShortItem(tn, sn));
 			}
 
 		}
 		return itemList;
-	}
-}
-
-[Serializable]
-public class ShortKeyCodeItem
-{
-	public string TargetName;
-	public string ShortKeyName;
-
-	public string MenuItemName
-	{
-		get
-		{
-			if (string.IsNullOrEmpty(TargetName))
-				return null;
-			return TargetName.Substring(TargetName.LastIndexOf('/')+1).Replace(" ","");
-		}
-	}
-
-	public string FuncName
-	{
-		get
-		{
-			if (string.IsNullOrEmpty(TargetName))
-				return null;
-			return TargetName.Replace("/", "").Replace(" ","").Replace("...","").Replace("&","").Replace("#","");
-		}
-	}
-
-	public ShortKeyCodeItem()
-	{
-		TargetName = "";
-		ShortKeyName = "";
-	}
-
-	public ShortKeyCodeItem(string _targetName,string _shortcutName)
-	{
-		TargetName = "";
-		ShortKeyName = "";
-		TargetName = _targetName;
-		ShortKeyName = _shortcutName;
 	}
 }
